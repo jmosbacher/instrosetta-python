b71987f794b4cc7efcf32dc3495f3ef9bda38086
Yossi Mosbacher <joe.mosbacher@gmail.com>
2019-03-16 15:42:31 +0200

    clean up singleaxis client

--- instrosetta/clients/motion/singleaxis.py
+++ instrosetta/clients/motion/singleaxis.py
@@ -2,74 +2,39 @@ import grpc
 from enum import Enum
 import pint
 from instrosetta.utils.units import accept_text
-from instrosetta.interfaces.motion import singleaxis_pb2
-from instrosetta.interfaces.motion import singleaxis_pb2_grpc
+from instrosetta.interfaces.motion import pb2 as pb2 
+from instrosetta.interfaces.motion import pb2_grpc as pb2_grpc
 from instrosetta.client import RpcClient
 ureg = pint.UnitRegistry()
 Q_ = ureg.Quantity
 
 
 class SingleAxis(RpcClient):
-    def __init__(self, addr="localhost:50052"):
-        self.addr = addr 
-        self._channel = None
-        self._stub = None
-       
-    def _single_rpc(self, method, request):
-        try:
-            resp = getattr(self._stub, method)(request)
-            return resp
-        except grpc.RpcError as e:
-            print(e)
-            # FIXME: log error/ raise exception.
+    stub_class = pb2_grpc.SingleAxisStub
 
-    def single_rpc(self, method, request):
-        if self._channel is None:
-            with self as s:
-                return s._single_rpc(method, request)
-        else:
-            return self._single_rpc(method, request)
-
-    def _streaming_rpc(self, method, request):
-        try:
-            for resp in getattr(self._stub, method)(request):
-                yield resp
-        except grpc.RpcError as e:
-            print(e)
-            # FIXME: log error/ raise exception.
-            #    
-    def streaming_rpc(self, method, request):
-        if self._channel is None:
-            with self as s:
-                for resp in s._streaming_rpc(method, request):
-                    yield resp
-        else:
-            for resp in self._streaming_rpc(method, request):
-                    yield resp
-            
     def echo(self, text):
-        req = singleaxis_pb2.TextMessage(content=text)
+        req = pb2.TextMessage(content=text)
         return self.single_rpc("Echo", req).content
 
     def scan_devices(self):
-        req = singleaxis_pb2.ScanDevicesRequest()
+        req = pb2.ScanDevicesRequest()
         return [resp.serial_number for resp in self.streaming_rpc('ScanDevices', req)]
 
     def connect(self, serial_number, motor_type=0, timeout=5, polling_interval=0.25):
-        dev = singleaxis_pb2.Device(serial_number=serial_number, motor_type=motor_type)
-        req = singleaxis_pb2.ConnectRequest(device=dev, timeout=timeout, polling_interval=polling_interval)
+        dev = pb2.Device(serial_number=serial_number, motor_type=motor_type)
+        req = pb2.ConnectRequest(device=dev, timeout=timeout, polling_interval=polling_interval)
         self.single_rpc("Connect", req)
 
     def disconnect(self):
-        req = singleaxis_pb2.DisconnectRequest()
+        req = pb2.DisconnectRequest()
         self.single_rpc("Disconnect", req)
 
     def home(self):
-        req = singleaxis_pb2.HomeMotorRequest()
+        req = pb2.HomeMotorRequest()
         self.single_rpc("HomeMotor", req)
 
     def get_range(self, units='mm'):
-        req = singleaxis_pb2.GetRangeRequest(units=units)
+        req = pb2.GetRangeRequest(units=units)
         resp = self.single_rpc("GetRange", req)
         if resp is not None:
             return {"minimum": Q_(resp.min, resp.units),
@@ -83,7 +48,7 @@ class SingleAxis(RpcClient):
 
     @property
     def position(self):
-        req = singleaxis_pb2.GetPositionRequest()
+        req = pb2.GetPositionRequest()
         resp = self.single_rpc("GetPosition", req)
         if resp is not None:
             return Q_(resp.value, resp.units)
@@ -93,30 +58,20 @@ class SingleAxis(RpcClient):
     @position.setter       
     @accept_text
     def position(self, destination):
-        pos = singleaxis_pb2.Position(value=destination.magnitude, units=str(destination.units))
-        req = singleaxis_pb2.MoveAbsoluteRequest(position=pos)
+        pos = pb2.Position(value=destination.magnitude, units=str(destination.units))
+        req = pb2.MoveAbsoluteRequest(position=pos)
         [Q_(resp.value, resp.units) for resp in self.streaming_rpc("MoveAbsolute", req)]
 
     @accept_text
     def move_absolute(self, destination):
-        pos = singleaxis_pb2.Position(value=destination.magnitude, units=str(destination.units))
-        req = singleaxis_pb2.MoveAbsoluteRequest(position=pos)
+        pos = pb2.Position(value=destination.magnitude, units=str(destination.units))
+        req = pb2.MoveAbsoluteRequest(position=pos)
         track = [(Q_(float('nan')) if resp is None else Q_(resp.value, resp.units)) for resp in self.streaming_rpc("MoveAbsolute", req)]
         return track
 
     @accept_text
     def move_relative(self, distance, direction=1):
-        dist = singleaxis_pb2.Distance(value=distance.magnitude, units=str(distance.units), direction=direction)
-        req = singleaxis_pb2.MoveRelativeRequest(distance=dist)
+        dist = pb2.Distance(value=distance.magnitude, units=str(distance.units), direction=direction)
+        req = pb2.MoveRelativeRequest(distance=dist)
         track = [(Q_(float('nan')) if resp is None else Q_(resp.value, resp.units)) for resp in self.streaming_rpc("MoveRelative", req)]
         return track
-
-    def __enter__(self):
-        self._channel = grpc.insecure_channel(self.addr)
-        self._stub = singleaxis_pb2_grpc.SingleAxisStub(self._channel)
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self._channel.close()
-        self._channel = None
-        self._stub = None
