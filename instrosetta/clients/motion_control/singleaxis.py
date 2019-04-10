621fd3119c537b49119cfc4aba558bd05e84f0d6
Yossi Mosbacher <joe.mosbacher@gmail.com>
2019-04-10 14:42:50 +0300

    import from common

--- instrosetta/clients/motion_control/singleaxis.py
+++ instrosetta/clients/motion_control/singleaxis.py
@@ -2,8 +2,9 @@ import grpc
 from enum import Enum
 import pint
 from instrosetta.utils.units import accept_text
-from instrosetta.interfaces.motion_control import singleaxis_pb2 as pb2 
+from instrosetta.interfaces.motion_control import singleaxis_pb2 as pb2
 from instrosetta.interfaces.motion_control import singleaxis_pb2_grpc as pb2_grpc
+from instrosetta.common import connection_pb2
 from instrosetta.client import RpcClient
 ureg = pint.UnitRegistry()
 Q_ = ureg.Quantity
@@ -17,11 +18,11 @@ class SingleAxis(RpcClient):
         return [resp.device_id for resp in self.streaming_rpc('ScanDevices', req)]
 
     def connect(self, device_id='', timeout=5):
-        req = pb2.ConnectRequest(device_id=device_id, timeout=timeout)
+        req = connection_pb2.ConnectRequest(device_id=device_id, timeout=timeout)
         self.single_rpc("Connect", req)
 
     def disconnect(self):
-        req = pb2.DisconnectRequest()
+        req = connection_pb2.DisconnectRequest()
         self.single_rpc("Disconnect", req)
 
     def home(self):
